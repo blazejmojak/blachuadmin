2dab845470c20dcac34d19eb56b18d7b02bbb78f
blachu <blazejmojak@gmail.com>
2021-07-25 11:41:56 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Products from './components/Products'
 import ProductDetails from './components/ProductDetails';
 
+axios.defaults.withCredentials = true
+
 function App() {
   return (
     <Router>
